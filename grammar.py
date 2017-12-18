0d4ded768cbf4582df851df29ce924da97f497a6
ifratric <ifratric@google.com>
2017-12-18 17:14:26 +0100

    add jscript grammar and generator

--- grammar.py
+++ grammar.py
@@ -282,7 +282,7 @@ class Grammar(object):
         while len(context['lines']) < num_lines:
             tmp_context = context.copy()
             try:
-                if random.random() < self._interesting_line_prob:
+                if (random.random() < self._interesting_line_prob) and (len(tmp_context['interesting_lines']) > 0):
                     tmp_context['force_var_reuse'] = True
                     lineno = random.choice(tmp_context['interesting_lines'])
                 else:
---
+++ jscript/README.md
@@ -0,0 +1,3 @@
+Script and grammar for fuzzing Microsoft jscript.dll JavaScript engine.
+
+Usage is the same as for DOM fuzzing.
