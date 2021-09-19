0d1895968fcfa1f36d9eecd7ec2b0ab0bab7c0a4
GawdOfROFL <gawdofrofl@protonmail.com>
2021-09-19 18:16:10 +0530

    cleaned the repo

--- generator.py
+++ generator.py
@@ -1,266 +1,10 @@
-#   Domato - main generator script
-#   -------------------------------
-#
-#   Written and maintained by Ivan Fratric <ifratric@google.com>
-#
-#   Copyright 2017 Google Inc. All Rights Reserved.
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-
-
 from __future__ import print_function
-import os
 import re
 import random
-import sys
 
 from grammar import Grammar
-
-_N_MAIN_LINES = 1000
-_N_EVENTHANDLER_LINES = 500
-
-_N_ADDITIONAL_HTMLVARS = 5
-
-# A map from tag name to corresponding type for HTML tags
-_HTML_TYPES = {
-    'a': 'HTMLAnchorElement',
-    'abbr': 'HTMLUnknownElement',
-    'acronym': 'HTMLUnknownElement',
-    'address': 'HTMLUnknownElement',
-    'applet': 'HTMLUnknownElement',
-    'area': 'HTMLAreaElement',
-    'article': 'HTMLUnknownElement',
-    'aside': 'HTMLUnknownElement',
-    'audio': 'HTMLAudioElement',
-    'b': 'HTMLUnknownElement',
-    'base': 'HTMLBaseElement',
-    'basefont': 'HTMLUnknownElement',
-    'bdi': 'HTMLUnknownElement',
-    'bdo': 'HTMLUnknownElement',
-    'bgsound': 'HTMLUnknownElement',
-    'big': 'HTMLUnknownElement',
-    'blockquote': 'HTMLUnknownElement',
-    'br': 'HTMLBRElement',
-    'button': 'HTMLButtonElement',
-    'canvas': 'HTMLCanvasElement',
-    'caption': 'HTMLTableCaptionElement',
-    'center': 'HTMLUnknownElement',
-    'cite': 'HTMLUnknownElement',
-    'code': 'HTMLUnknownElement',
-    'col': 'HTMLTableColElement',
-    'colgroup': 'HTMLUnknownElement',
-    'command': 'HTMLUnknownElement',
-    'content': 'HTMLContentElement',
-    'data': 'HTMLDataElement',
-    'datalist': 'HTMLDataListElement',
-    'dd': 'HTMLUnknownElement',
-    'del': 'HTMLModElement',
-    'details': 'HTMLDetailsElement',
-    'dfn': 'HTMLUnknownElement',
-    'dialog': 'HTMLDialogElement',
-    'dir': 'HTMLDirectoryElement',
-    'div': 'HTMLDivElement',
-    'dl': 'HTMLDListElement',
-    'dt': 'HTMLUnknownElement',
-    'em': 'HTMLUnknownElement',
-    'embed': 'HTMLEmbedElement',
-    'fieldset': 'HTMLFieldSetElement',
-    'figcaption': 'HTMLUnknownElement',
-    'figure': 'HTMLUnknownElement',
-    'font': 'HTMLFontElement',
-    'footer': 'HTMLUnknownElement',
-    'form': 'HTMLFormElement',
-    'frame': 'HTMLFrameElement',
-    'frameset': 'HTMLFrameSetElement',
-    'h1': 'HTMLHeadingElement',
-    'h2': 'HTMLHeadingElement',
-    'h3': 'HTMLHeadingElement',
-    'h4': 'HTMLHeadingElement',
-    'h5': 'HTMLHeadingElement',
-    'h6': 'HTMLHeadingElement',
-    'header': 'HTMLUnknownElement',
-    'hgroup': 'HTMLUnknownElement',
-    'hr': 'HTMLHRElement',
-    'i': 'HTMLUnknownElement',
-    'iframe': 'HTMLIFrameElement',
-    'image': 'HTMLImageElement',
-    'img': 'HTMLImageElement',
-    'input': 'HTMLInputElement',
-    'ins': 'HTMLModElement',
-    'isindex': 'HTMLUnknownElement',
-    'kbd': 'HTMLUnknownElement',
-    'keygen': 'HTMLKeygenElement',
-    'label': 'HTMLLabelElement',
-    'layer': 'HTMLUnknownElement',
-    'legend': 'HTMLLegendElement',
-    'li': 'HTMLLIElement',
-    'link': 'HTMLLinkElement',
-    'listing': 'HTMLUnknownElement',
-    'main': 'HTMLUnknownElement',
-    'map': 'HTMLMapElement',
-    'mark': 'HTMLUnknownElement',
-    'marquee': 'HTMLMarqueeElement',
-    'menu': 'HTMLMenuElement',
-    'menuitem': 'HTMLMenuItemElement',
-    'meta': 'HTMLMetaElement',
-    'meter': 'HTMLMeterElement',
-    'nav': 'HTMLUnknownElement',
-    'nobr': 'HTMLUnknownElement',
-    'noembed': 'HTMLUnknownElement',
-    'noframes': 'HTMLUnknownElement',
-    'nolayer': 'HTMLUnknownElement',
-    'noscript': 'HTMLUnknownElement',
-    'object': 'HTMLObjectElement',
-    'ol': 'HTMLOListElement',
-    'optgroup': 'HTMLOptGroupElement',
-    'option': 'HTMLOptionElement',
-    'output': 'HTMLOutputElement',
-    'p': 'HTMLParagraphElement',
-    'param': 'HTMLParamElement',
-    'picture': 'HTMLPictureElement',
-    'plaintext': 'HTMLUnknownElement',
-    'pre': 'HTMLPreElement',
-    'progress': 'HTMLProgressElement',
-    'q': 'HTMLQuoteElement',
-    'rp': 'HTMLUnknownElement',
-    'rt': 'HTMLUnknownElement',
-    'ruby': 'HTMLUnknownElement',
-    's': 'HTMLUnknownElement',
-    'samp': 'HTMLUnknownElement',
-    'section': 'HTMLUnknownElement',
-    'select': 'HTMLSelectElement',
-    'shadow': 'HTMLShadowElement',
-    'small': 'HTMLUnknownElement',
-    'source': 'HTMLSourceElement',
-    'span': 'HTMLSpanElement',
-    'strike': 'HTMLUnknownElement',
-    'strong': 'HTMLUnknownElement',
-    'style': 'HTMLStyleElement',
-    'sub': 'HTMLUnknownElement',
-    'summary': 'HTMLUnknownElement',
-    'sup': 'HTMLUnknownElement',
-    'table': 'HTMLTableElement',
-    'tbody': 'HTMLTableSectionElement',
-    'td': 'HTMLUnknownElement',
-    'template': 'HTMLTemplateElement',
-    'textarea': 'HTMLTextAreaElement',
-    'tfoot': 'HTMLTableSectionElement',
-    'th': 'HTMLTableCellElement',
-    'thead': 'HTMLTableSectionElement',
-    'time': 'HTMLTimeElement',
-    'title': 'HTMLTitleElement',
-    'tr': 'HTMLTableRowElement',
-    'track': 'HTMLTrackElement',
-    'tt': 'HTMLUnknownElement',
-    'u': 'HTMLUnknownElement',
-    'ul': 'HTMLUListElement',
-    'var': 'HTMLUnknownElement',
-    'video': 'HTMLVideoElement',
-    'wbr': 'HTMLUnknownElement',
-    'xmp': 'HTMLUnknownElement'
-}
-
-# A map from tag name to corresponding type for SVG tags
-_SVG_TYPES = {
-    'a': 'SVGAElement',
-    'altGlyph': 'SVGElement',
-    'altGlyphDef': 'SVGElement',
-    'altGlyphItem': 'SVGElement',
-    'animate': 'SVGAnimateElement',
-    'animateColor': 'SVGElement',
-    'animateMotion': 'SVGAnimateMotionElement',
-    'animateTransform': 'SVGAnimateTransformElement',
-    'circle': 'SVGCircleElement',
-    'clipPath': 'SVGClipPathElement',
-    'color-profile': 'SVGElement',
-    'cursor': 'SVGCursorElement',
-    'defs': 'SVGDefsElement',
-    'desc': 'SVGDescElement',
-    'discard': 'SVGDiscardElement',
-    'ellipse': 'SVGEllipseElement',
-    'feBlend': 'SVGFEBlendElement',
-    'feColorMatrix': 'SVGFEColorMatrixElement',
-    'feComponentTransfer': 'SVGFEComponentTransferElement',
-    'feComposite': 'SVGFECompositeElement',
-    'feConvolveMatrix': 'SVGFEConvolveMatrixElement',
-    'feDiffuseLighting': 'SVGFEDiffuseLightingElement',
-    'feDisplacementMap': 'SVGFEDisplacementMapElement',
-    'feDistantLight': 'SVGFEDistantLightElement',
-    'feDropShadow': 'SVGFEDropShadowElement',
-    'feFlood': 'SVGFEFloodElement',
-    'feFuncA': 'SVGFEFuncAElement',
-    'feFuncB': 'SVGFEFuncBElement',
-    'feFuncG': 'SVGFEFuncGElement',
-    'feFuncR': 'SVGFEFuncRElement',
-    'feGaussianBlur': 'SVGFEGaussianBlurElement',
-    'feImage': 'SVGFEImageElement',
-    'feMerge': 'SVGFEMergeElement',
-    'feMergeNode': 'SVGFEMergeNodeElement',
-    'feMorphology': 'SVGFEMorphologyElement',
-    'feOffset': 'SVGFEOffsetElement',
-    'fePointLight': 'SVGFEPointLightElement',
-    'feSpecularLighting': 'SVGFESpecularLightingElement',
-    'feSpotLight': 'SVGFESpotLightElement',
-    'feTile': 'SVGFETileElement',
-    'feTurbulence': 'SVGFETurbulenceElement',
-    'filter': 'SVGFilterElement',
-    'font': 'SVGElement',
-    'font-face': 'SVGElement',
-    'font-face-format': 'SVGElement',
-    'font-face-name': 'SVGElement',
-    'font-face-src': 'SVGElement',
-    'font-face-uri': 'SVGElement',
-    'foreignObject': 'SVGForeignObjectElement',
-    'g': 'SVGGElement',
-    'glyph': 'SVGElement',
-    'glyphRef': 'SVGElement',
-    'hatch': 'SVGElement',
-    'hatchpath': 'SVGElement',
-    'hkern': 'SVGElement',
-    'image': 'SVGImageElement',
-    'line': 'SVGLineElement',
-    'linearGradient': 'SVGLinearGradientElement',
-    'marker': 'SVGMarkerElement',
-    'mask': 'SVGMaskElement',
-    'mesh': 'SVGElement',
-    'meshgradient': 'SVGElement',
-    'meshpatch': 'SVGElement',
-    'meshrow': 'SVGElement',
-    'metadata': 'SVGMetadataElement',
-    'missing-glyph': 'SVGElement',
-    'mpath': 'SVGMPathElement',
-    'path': 'SVGPathElement',
-    'pattern': 'SVGPatternElement',
-    'polygon': 'SVGPolygonElement',
-    'polyline': 'SVGPolylineElement',
-    'radialGradient': 'SVGRadialGradientElement',
-    'rect': 'SVGRectElement',
-    'set': 'SVGSetElement',
-    'svg': 'SVGSVGElement',
-    'solidcolor': 'SVGElement',
-    'stop': 'SVGStopElement',
-    'switch': 'SVGSwitchElement',
-    'symbol': 'SVGSymbolElement',
-    'text': 'SVGTextElement',
-    'textPath': 'SVGTextPathElement',
-    'title': 'SVGTitleElement',
-    'tref': 'SVGElement',
-    'tspan': 'SVGTSpanElement',
-    'unknown': 'SVGElement',
-    'use': 'SVGUseElement',
-    'view': 'SVGViewElement',
-    'vkern': 'SVGElement'
-}
-
+from svg_tags import _SVG_TYPES
+from html_tags import _HTML_TYPES
 
 def generate_html_elements(ctx, n):
     for i in range(n):
@@ -304,7 +48,6 @@ def generate_function_body(jsgrammar, htmlctx, num_lines):
 
 def check_grammar(grammar):
     """Checks if grammar has errors and if so outputs them.
-
     Args:
       grammar: The grammar to check.
     """
@@ -321,13 +64,11 @@ def check_grammar(grammar):
 
 def generate_new_sample(template, htmlgrammar, cssgrammar, jsgrammar):
     """Parses grammar rules from string.
-
     Args:
       template: A template string.
       htmlgrammar: Grammar for generating HTML code.
       cssgrammar: Grammar for generating CSS code.
       jsgrammar: Grammar for generating JS code.
-
     Returns:
       A string containing sample data.
     """
@@ -348,16 +89,16 @@ def generate_new_sample(template, htmlgrammar, cssgrammar, jsgrammar):
         lambda match: add_html_ids(match, htmlctx),
         html
     )
-    generate_html_elements(htmlctx, _N_ADDITIONAL_HTMLVARS)
+    generate_html_elements(htmlctx, 5)
 
     result = result.replace('<cssfuzzer>', css)
     result = result.replace('<htmlfuzzer>', html)
 
     handlers = False
     while '<jsfuzzer>' in result:
-        numlines = _N_MAIN_LINES
+        numlines = 1000
         if handlers:
-            numlines = _N_EVENTHANDLER_LINES
+            numlines = 500
         else:
             handlers = True
         result = result.replace(
@@ -368,38 +109,27 @@ def generate_new_sample(template, htmlgrammar, cssgrammar, jsgrammar):
 
     return result
 
-
-def generate_samples(grammar_dir, outfiles):
-    """Generates a set of samples and writes them to the output files.
-
-    Args:
-      grammar_dir: directory to load grammar files from.
-      outfiles: A list of output filenames.
-    """
-
-    f = open(os.path.join(grammar_dir, 'template.html'))
-    template = f.read()
-    f.close()
-
+def import_grammar():
+    
     htmlgrammar = Grammar()
-    err = htmlgrammar.parse_from_file(os.path.join(grammar_dir, 'html.txt'))
+    err = htmlgrammar.parse_from_file('rules/html.txt')
     # CheckGrammar(htmlgrammar)
     if err > 0:
-        print('There were errors parsing grammar')
+        print('There were errors parsing html grammar')
         return
 
     cssgrammar = Grammar()
-    err = cssgrammar.parse_from_file(os.path.join(grammar_dir, 'css.txt'))
+    err = cssgrammar.parse_from_file('rules/css.txt')
     # CheckGrammar(cssgrammar)
     if err > 0:
-        print('There were errors parsing grammar')
+        print('There were errors parsing css grammar')
         return
 
     jsgrammar = Grammar()
-    err = jsgrammar.parse_from_file(os.path.join(grammar_dir, 'js.txt'))
+    err = jsgrammar.parse_from_file('rules/js.txt')
     # CheckGrammar(jsgrammar)
     if err > 0:
-        print('There were errors parsing grammar')
+        print('There were errors parsing js grammar')
         return
 
     # JS and HTML grammar need access to CSS grammar.
@@ -407,67 +137,12 @@ def generate_samples(grammar_dir, outfiles):
     htmlgrammar.add_import('cssgrammar', cssgrammar)
     jsgrammar.add_import('cssgrammar', cssgrammar)
 
-    for outfile in outfiles:
-        result = generate_new_sample(template, htmlgrammar, cssgrammar,
-                                     jsgrammar)
+    return htmlgrammar, cssgrammar, jsgrammar
 
-        if result is not None:
-            print('Writing a sample to ' + outfile)
-            try:
-                f = open(outfile, 'w')
-                f.write(result)
-                f.close()
-            except IOError:
-                print('Error writing to output')
-
-
-def get_option(option_name):
-    for i in range(len(sys.argv)):
-        if (sys.argv[i] == option_name) and ((i + 1) < len(sys.argv)):
-            return sys.argv[i + 1]
-        elif sys.argv[i].startswith(option_name + '='):
-            return sys.argv[i][len(option_name) + 1:]
-    return None
-
-
-def main():
-    fuzzer_dir = os.path.dirname(__file__)
-
-    multiple_samples = False
-
-    for a in sys.argv:
-        if a.startswith('--output_dir='):
-            multiple_samples = True
-
-    if '--output_dir' in sys.argv:
-        multiple_samples = True
-
-    if multiple_samples:
-        print('Running on ClusterFuzz')
-        out_dir = get_option('--output_dir')
-        nsamples = int(get_option('--no_of_files'))
-        print('Output directory: ' + out_dir)
-        print('Number of samples: ' + str(nsamples))
-
-        if not os.path.exists(out_dir):
-            os.mkdir(out_dir)
-
-        outfiles = []
-        for i in range(nsamples):
-            outfiles.append(os.path.join(out_dir, 'fuzz-' + str(i).zfill(5) + '.html'))
-
-        generate_samples(fuzzer_dir, outfiles)
-
-    elif len(sys.argv) > 1:
-        outfile = sys.argv[1]
-        generate_samples(fuzzer_dir, [outfile])
-
-    else:
-        print('Arguments missing')
-        print("Usage:")
-        print("\tpython generator.py <output file>")
-        print("\tpython generator.py --output_dir <output directory> --no_of_files <number of output files>")
-
-
-if __name__ == '__main__':
-    main()
+def generate_samples(template):
+    """Generates a set of samples and writes them to the output files.
+    Args:
+      grammar_dir: directory to load grammar files from.
+      outfiles: A list of output filenames.
+    """
+    return generate_new_sample(template, *import_grammar())
---
+++ main.py
@@ -0,0 +1,68 @@
+import generator
+import argparse
+import os
+
+def get_parser():
+    
+    parser = argparse.ArgumentParser(description="DOMATO (A DOM FUZZER)")
+    
+    parser.add_argument("-f", "--file", 
+    help="File name which is to be generated in the same directory")
+
+    parser.add_argument('-o', '--output_dir', type=str,
+                    help='The output directory to put the generated files in')
+
+    parser.add_argument('-n', '--no_of_files', type=int,
+                    help='number of files to be generated')
+
+    return parser
+
+def main():
+
+    with open("template.html", "r") as f:
+            template = f.read()
+            f.close()
+
+    parser = get_parser()
+    
+    args = parser.parse_args()
+
+    result = generator.generate_samples(template)
+
+    if args.file:
+        with open(args.file, "w") as f:
+            f.write(result)
+
+    elif args.output_dir:
+        if not args.no_of_files:
+            print("Please use switch -n to specify the number of files")
+        else:
+            print('Running on ClusterFuzz')
+            out_dir = args.output_dir
+            nsamples = args.no_of_files
+            print('Output directory: ' + out_dir)
+            print('Number of samples: ' + str(nsamples))
+
+            if not os.path.exists(out_dir):
+                os.mkdir(out_dir)
+
+            outfiles = []
+            for i in range(nsamples):
+                outfiles.append(os.path.join(out_dir, 'fuzz-' + str(i).zfill(5) + '.html'))
+
+            for outfile in outfiles:
+                if result is not None:
+                    print('Writing a sample to ' + outfile)
+                try:
+                    with open(outfile, 'w') as f:
+                        f.write(result)
+                        f.close()
+                except IOError:
+                    print('Error writing to output')
+
+    else:
+        parser.print_help()
+
+
+if __name__ == '__main__':
+    main()
